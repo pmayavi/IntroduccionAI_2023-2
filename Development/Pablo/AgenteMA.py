193cc2ccec40d6652ab3509045037b04b4fbcde6
pmayavi <pmayav@eafit.edu.co>
2023-08-27 14:13:17 -0500

    Validacion y es funcional!

--- Development/Pablo/AgenteMA.py
+++ Development/Pablo/AgenteMA.py
@@ -2,14 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import threading
-from selenium.common.exceptions import StaleElementReferenceException
+import selenium.common.exceptions as ex
 
 def auto_timmer():
     tiempo_inicio = time.time()
     def timer():
         while True:
             minutos, segundos = divmod(int(time.time() - tiempo_inicio), 60)
-            print(f"Tiempo transcurrido: {minutos:02d}:{segundos:02d}", end="\r")
+            print(f"                                                         Tiempo transcurrido: {minutos:02d}:{segundos:02d}", end="\r\r")
             time.sleep(1)
     thread = threading.Thread(target=timer)
     thread.daemon = True  # Set the thread as daemon so it will exit when the main program exits
@@ -46,7 +46,7 @@ def insertar(nodo_actual, nuevo_elemento, es_interactuable):
             nodo_actual.elemento.append(nuevo_elemento)
         else:
             nodo_actual.derecha = insertar(nodo_actual.derecha, nuevo_elemento, es_interactuable)
-    except StaleElementReferenceException:
+    except ex.StaleElementReferenceException:
         print("Stale Element Reference Exception.")
         nodo_actual.derecha = insertar(nodo_actual.derecha, nuevo_elemento, es_interactuable)
 
@@ -63,52 +63,33 @@ def get_index(elemento):
         elemento.i = 0
         if elemento.tag_name in switch:
             elemento.i += switch[elemento.tag_name]
-    except StaleElementReferenceException:
+    except ex.StaleElementReferenceException:
         print("Stale Element Reference Exception.")
 
 def buscar_interactuable(nodo_actual, tag, i):
     if nodo_actual is None:
         return False
     
-    if nodo_actual.elemento[0] and nodo_actual.elemento[0].tag_name == tag or nodo_actual.elemento[0].i == i:
-        return nodo_actual.elemento[0]
+    if nodo_actual.elemento[0] and nodo_actual.elemento[0].tag_name == tag:
+        return nodo_actual
     
     if nodo_actual.elemento and i < nodo_actual.elemento[0].i:
         return buscar_interactuable(nodo_actual.izquierda, tag, i)
     else:
         return buscar_interactuable(nodo_actual.derecha, tag, i)
-    
-def validar_id_busqueda(elemento):
-    palabras_busqueda=["Search", "search", "Búsqueda", "búsqueda", "busqueda", "Busqueda", "Buscar", "buscar"]
-    try:
-        if elemento.id and any(palabra in str(elemento.id) for palabra in palabras_busqueda):
-            return True
-    except StaleElementReferenceException:
-        print("Stale Element Reference Exception.")
-    return False
-    
-def validar_text_busqueda(elemento):
-    palabras_busqueda=["Search", "search", "Búsqueda", "búsqueda", "busqueda", "Busqueda", "Buscar", "buscar"]
-    try:
-        if elemento.text and any(palabra in elemento.text for palabra in palabras_busqueda):
-            return True
-    except StaleElementReferenceException:
-        print("Stale Element Reference Exception.")
-    return False
 
-def validar_label_busqueda(elemento):
-    palabras_busqueda=["Search", "search", "Búsqueda", "búsqueda", "busqueda", "Busqueda", "Buscar", "buscar"]
-    if elemento.get_attribute("label"):
-        print(elemento.get_attribute("label"))
+def validar_busqueda(elemento):
+    set1 = set(["Search", "search", "Búsqueda", "búsqueda", "busqueda", "Busqueda", "Buscar", "buscar"])
     try:
-        if elemento.get_attribute("label") and any(palabra in elemento.get_attribute("label") for palabra in palabras_busqueda):
-            return True
-    except StaleElementReferenceException:
+        set2 = set([elemento.get_attribute("label"), elemento.text, elemento.id, elemento.get_attribute("id"), elemento.get_attribute("placeholder") ])
+        common_elements = set1.intersection(set2)
+        return len(common_elements) > 0
+    except ex.StaleElementReferenceException:
         print("Stale Element Reference Exception.")
-    return False
-
+        return False
+    
 def main():
-    #auto_timmer()
+    auto_timmer()
     # Configuración de Selenium
     driver = webdriver.Chrome()
     url = "https://www.youtube.com"
@@ -130,16 +111,16 @@ def main():
         # Aquí puedes agregar lógica para determinar si la elemento es interactuable
         # Por ejemplo, si es un enlace (<a>) o un botón (<button>)
         try:
-            if validar_id_busqueda(elemento) or validar_text_busqueda(elemento) or validar_label_busqueda(elemento):
+            if validar_busqueda(elemento):
                 elemento.i = 4
                 es_interactuable = True
                 raiz = insertar(raiz, elemento, es_interactuable)
-                all += "Search: " + elemento.text + " " + elemento.id +  " "+ str(elemento.i) +"\n"
+                all += "Search: " + elemento.text + " "+ str(elemento.i) +"\n"
             elif elemento.tag_name in ["a", "button", "input", "textarea"]:
                 es_interactuable = True
                 raiz = insertar(raiz, elemento, es_interactuable)
-                all += elemento.tag_name + ": " + elemento.text + " " + elemento.id + " "+ str(elemento.i) +"\n"
-        except StaleElementReferenceException:
+                all += elemento.tag_name + ": " + elemento.text + " "+ str(elemento.i) +"\n"
+        except ex.StaleElementReferenceException:
             print("Stale Element Reference Exception.")
             
 
@@ -154,12 +135,16 @@ def main():
         print("El árbol está vacío.")
 
     # Buscar si una elemento específica es interactuable
-    elemento_buscada = "ydt-searchbox"
+    elemento_buscada = "ytd-searchbox"
     es_interactuable = buscar_interactuable(raiz, elemento_buscada, 4)
 
     if es_interactuable:
-        print(f"El elemento <{elemento_buscada}> es interactuable.")
-        es_interactuable.send_keys("Tetas hombres")
+        for elemento in es_interactuable.elemento:
+            try:
+                print(f"El elemento <{elemento_buscada}> es interactuable.")
+                elemento.send_keys("Tetas hombres")
+            except ex.ElementNotInteractableException:
+                print("No es esta")
     else:
         print(f"El elemento <{elemento_buscada}> no es interactuable.")
 
